2077a1936107a0671c6d3cb64d46832f7332ab92
Claas <claas.moehlmann@outlook.com>
2023-03-06 19:07:50 +0100

    Switch to vitest
    because the playwright component tests are experimental and this might allow for other frontend tests besides it

--- web/tests/SignIn.test.ts
+++ web/tests/SignIn.test.ts
@@ -1,64 +1,81 @@
-import { test, expect } from "@playwright/experimental-ct-svelte";
 import SignIn from "../src/SignIn.svelte";
+import { beforeAll, afterAll, test, it, describe } from "vitest";
+import type { PreviewServer } from "vite";
+import type { Browser, Page } from "playwright";
+import { expect } from "@playwright/test";
+import { preview } from "vite";
+import { chromium } from "playwright";
 
-const usernameInputSelector = "input[autocomplete=username]";
-const passwordInputSelector = "input[autocomplete=current-password]";
+describe("Sign in", () => {
+  const usernameInputSelector = "input[autocomplete=username]";
+  const passwordInputSelector = "input[autocomplete=current-password]";
 
-test("Sign in has username and password input", async ({ mount }) => {
-  // Arrange
-  const component = await mount(SignIn);
-  const usernameInput = await component
-    .locator(usernameInputSelector)
-    .elementHandle();
+  // Tests should not not depend on each other, so avoid implicitly passed state like this
+  let page: Page;
+  beforeAll(async () => {
+    const port = 3000;
+    const server = await preview({ preview: { port } });
+    const browser = await chromium.launch();
+    page = await browser.newPage();
+    await page.goto(`http://localhost:${port}/signin`);
 
-  const passwordInput = await component
-    .locator(passwordInputSelector)
-    .elementHandle();
+    return async () => {
+      await browser.close();
+      await new Promise<void>((resolve, reject) => {
+        server.httpServer.close((error) => (error ? reject(error) : resolve()));
+      });
+    };
+  });
 
-  // Assert
-  expect(usernameInput).not.toBeNull();
-  expect(passwordInput).not.toBeNull();
-});
+  it("Should have a username and password input", async () => {
+    // Arrange
+    const usernameInput = await page
+      .locator(usernameInputSelector)
+      .elementHandle();
 
-test("Sign in component password input should have password type attribute", async ({
-  mount,
-}) => {
-  // Arrange
-  const component = await mount(SignIn);
-  const typeAttribute = await component
-    .locator(passwordInputSelector)
-    .getAttribute("type");
+    const passwordInput = await page
+      .locator(passwordInputSelector)
+      .elementHandle();
 
-  // Assert
-  expect(typeAttribute).toBe("password");
-});
+    // Assert
+    expect(usernameInput).not.toBeNull();
+    expect(passwordInput).not.toBeNull();
+  });
 
-test("Sign in component username input should be required", async ({
-  mount,
-}) => {
-  // Arrange
-  const component = await mount(SignIn);
+  it("Should have a password input with the password type attribute", async () => {
+    // Arrange
+    const typeAttribute = await page
+      .locator(passwordInputSelector)
+      .getAttribute("type");
 
-  const requiredAttribute = await component
-    .locator(usernameInputSelector)
-    .getAttribute("required");
+    // Assert
+    expect(typeAttribute).toBe("password");
+  });
 
-  // Assert
-  // It's an empty attribute that just has to be there as a flag
-  expect(requiredAttribute).toBe("");
-});
+  it("Should have a username input that is required", async () => {
+    // Arrange
 
-test("Sign in component password input should be required", async ({
-  mount,
-}) => {
-  // Arrange
-  const component = await mount(SignIn);
+    const requiredAttribute = await page
+      .locator(usernameInputSelector)
+      .getAttribute("required");
 
-  const requiredAttribute = await component
-    .locator(passwordInputSelector)
-    .getAttribute("required");
+    // Assert
+    // It's an empty attribute that just has to be there as a flag
+    expect(requiredAttribute).toBe("");
+  });
 
-  // Assert
-  // It's an empty attribute that just has to be there as a flag
-  expect(requiredAttribute).toBe("");
+  it("Should have a password input that is required", async () => {
+    // Arrange
+    const requiredAttribute = await page
+      .locator(passwordInputSelector)
+      .getAttribute("required");
+
+    // Assert
+    // It's an empty attribute that just has to be there as a flag
+    expect(requiredAttribute).toBe("");
+  });
+
+  it.todo("Should have inputs with the same name as the server expexts");
+
+  it.todo("Should have a form that posts to the sign in enpoint");
 });
