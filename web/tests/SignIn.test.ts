f8ecf1ea35e29b1bf8719a2df61c133060932d60
Claas <claas.moehlmann@outlook.com>
2023-03-06 17:10:30 +0100

    Add tests for sign in page component
    Remove unused import
    Format configuration file
    Add css import for correct display

---
+++ web/tests/SignIn.test.ts
@@ -0,0 +1,64 @@
+import { test, expect } from "@playwright/experimental-ct-svelte";
+import SignIn from "../src/SignIn.svelte";
+
+const usernameInputSelector = "input[autocomplete=username]";
+const passwordInputSelector = "input[autocomplete=current-password]";
+
+test("Sign in has username and password input", async ({ mount }) => {
+  // Arrange
+  const component = await mount(SignIn);
+  const usernameInput = await component
+    .locator(usernameInputSelector)
+    .elementHandle();
+
+  const passwordInput = await component
+    .locator(passwordInputSelector)
+    .elementHandle();
+
+  // Act
+  // Assert
+  expect(usernameInput).not.toBeNull();
+  expect(passwordInput).not.toBeNull();
+});
+
+test("Sign in component password input should have password type attribute", async ({
+  mount,
+}) => {
+  // Arrange
+  const component = await mount(SignIn);
+  const typeAttribute = await component
+    .locator(passwordInputSelector)
+    .getAttribute("type");
+
+  // Assert
+  expect(typeAttribute).toBe("password");
+});
+
+test("Sign in component username input should be required", async ({
+  mount,
+}) => {
+  // Arrange
+  const component = await mount(SignIn);
+
+  const requiredAttribute = await component
+    .locator(usernameInputSelector)
+    .getAttribute("required");
+
+  // Assert
+  expect(requiredAttribute).not.toBeNull();
+});
+
+test("Sign in component password input should be required", async ({
+  mount,
+}) => {
+  // Arrange
+  const component = await mount(SignIn);
+
+  const requiredAttribute = await component
+    .locator(passwordInputSelector)
+    .getAttribute("required");
+
+  // Act
+  // Assert
+  expect(requiredAttribute).not.toBeNull();
+});
